d31ac767f2b5a357b225d9da7f0559d3226afd14
Gustavo Oliv <calacogustavo@gmail.com>
2026-04-13 21:28:43 -0300

    feat: cookies no cadastro

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,5 +1,6 @@
 import { Response } from "next-lib-utils";
 import { userService } from "@/services/userService";
+import { jwtUtil } from "@/utils/jwt";
 
 export async function POST(req) {
     try {
@@ -10,7 +11,17 @@ export async function POST(req) {
 
         const user = await userService.register(name, userName, password);
 
-        return Response.success({ user }, "Logado com sucesso", 201);
+        const token = jwtUtil.generate(user);
+
+        const response = Response.success({ user }, "Cadastrado com sucesso", 201);
+
+        response.cookies.set("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            path: "/"
+        });
+
+        return response;
     } catch (error) {
         return Response.error("Erro ao cadastrar", error);
     }
